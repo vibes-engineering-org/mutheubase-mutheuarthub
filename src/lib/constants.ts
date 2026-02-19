fa0c4f907cdb9f1619e354158ab148631ba25d17
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2026-02-19 12:43:12 +0000

    Initial commit from template

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const PROJECT_TITLE = "Mutheuarthub";
+export const PROJECT_DESCRIPTION = "by @mutheu.base.eth";
+export const PROJECT_CREATOR = "mutheu.base.eth";
+export const PROJECT_AVATAR_URL =
+  "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
